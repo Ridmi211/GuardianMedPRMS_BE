27c43d39a6d74d1fdda36e7165237f160b07b0ff
Ridmi211 <ridmiyatigammana@gmail.com>
2024-04-28 21:30:28 +0530

    Workflow

--- src/main/java/com/guardianMed/patientRecordManagement/system/repositories/PatientRepository.java
+++ src/main/java/com/guardianMed/patientRecordManagement/system/repositories/PatientRepository.java
@@ -4,8 +4,6 @@ package com.guardianMed.patientRecordManagement.system.repositories;
 import com.guardianMed.patientRecordManagement.system.models.Patient;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
-
-
 public interface PatientRepository extends MongoRepository<Patient, String> {
    Patient findByPatientName(String patientName);
    Patient findByPatientNIC(String patientNIC);
